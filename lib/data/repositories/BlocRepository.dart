7731f053813d62ff8ca3b2e6174006da16b39c36
fares-ubidata <131752202+fares-ubidata@users.noreply.github.com>
2023-05-10 14:25:00 +0200

    test pipe

--- lib/data/repositories/BlocRepository.dart
+++ lib/data/repositories/BlocRepository.dart
@@ -1,22 +1,22 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:rev/domain/models/bloc.dart';
-import 'package:rev/domain/repositories/IblocRepository.dart';
-import 'package:http/http.dart' as http;
+// import 'package:rev/domain/models/bloc.dart';
+// import 'package:rev/domain/repositories/IblocRepository.dart';
+// import 'package:http/http.dart' as http;
 
-class BlocRepository implements IBlocRepository {
-  final host = "https://10.0.2.2:5001/";
-  final Map<String, String> _headers = {
-    "Content-type": "application/json",
-    "Accept": "application/json"
-  };
-  @override
-  Future<Bloc> getBlocs() async {
-    var getUrl = host + "get";
-    var results = await http.get(Uri.parse(getUrl), headers: _headers);
-    final jsonObject = json.decode(results.body);
+// class BlocRepository implements IBlocRepository {
+//   final host = "https://10.0.2.2:5001/";
+//   final Map<String, String> _headers = {
+//     "Content-type": "application/json",
+//     "Accept": "application/json"
+//   };
+//   @override
+//   Future<Bloc> getBlocs() async {
+//     var getUrl = host + "get";
+//     var results = await http.get(Uri.parse(getUrl), headers: _headers);
+//     final jsonObject = json.decode(results.body);
 
-    var response = Bloc.fromJson(jsonObject);
-    return response;
-  }
-}
+//     var response = Bloc.fromJson(jsonObject);
+//     return response;
+//   }
+// }
